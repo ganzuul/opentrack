b702c778a7dcf9361d5a3b67a2a5106f4a09cbc8
ganzuul <tomas.rolin@gmail.com>
2026-03-17 16:57:44 +0200

    Fix copyright comment formatting in header file

--- filter-alpha-spectrum/alpha-spectrum-settings.hpp
+++ filter-alpha-spectrum/alpha-spectrum-settings.hpp
@@ -1,4 +1,4 @@
-"Copyright (C) 2026 ganzuul. This file is part of Alpha Spectrum. Alpha Spectrum is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, version 3..."
+// "Copyright (C) 2026 ganzuul. This file is part of Alpha Spectrum. Alpha Spectrum is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, version 3..."
 
 #pragma once
 
